f5a43dcdb00f76e3a0c2a3d3dff9a22b5d0c844b
arkwith7 <arkwith7@gmail.com>
2023-01-17 17:30:11 +0900

    20230117-0

--- aiocr/views.py
+++ aiocr/views.py
@@ -1,4 +1,45 @@
-from django.http import HttpResponse
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2023 - present arkwith
+"""
 
+from django import template
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.urls import reverse
+
+
+# @login_required(login_url="/login/")
 def index(request):
-    return HttpResponse("Hello! This is the aiocr APP index.")
+    return HttpResponse("Hello! This is the polls APP index.")
+    # context = {'segment': 'index'}
+
+    # html_template = loader.get_template('aiocr/index.html')
+    # return HttpResponse(html_template.render(context, request))
+
+
+# @login_required(login_url="/login/")
+def pages(request):
+    context = {}
+    # All resource paths end in .html.
+    # Pick out the html file name from the url. And load that template.
+    try:
+
+        load_template = request.path.split('/')[-1]
+
+        if load_template == 'admin':
+            return HttpResponseRedirect(reverse('admin:index'))
+        context['segment'] = load_template
+
+        html_template = loader.get_template('aiocr/' + load_template)
+        return HttpResponse(html_template.render(context, request))
+
+    except template.TemplateDoesNotExist:
+
+        html_template = loader.get_template('aiocr/page-404.html')
+        return HttpResponse(html_template.render(context, request))
+
+    except:
+        html_template = loader.get_template('aiocr/page-500.html')
+        return HttpResponse(html_template.render(context, request))
